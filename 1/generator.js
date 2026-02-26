c85824a393794ce042279e74cd5cff7c5479e977
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-02-26 15:51:52 +0200

    Init
    Початок роботи. Скоріш за все, окремо буде зроблена 1 лабораторна робота. Інші будуть зроблені в самому проєкті (Але це не точно)

---
+++ 1/generator.js
@@ -0,0 +1,12 @@
+//Генератор - пошагово генерує елементи об'єкта
+
+function fibonacciNumber(){
+    let counter = 0;
+    next(){
+        if(counter < 3){
+
+        }
+        return 1;
+    };
+
+}

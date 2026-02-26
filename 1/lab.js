41c87c8ca34aabb3ffbc55b5e452f00904291f39
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-02-26 18:54:36 +0200

    home stretch
    Мені не подобається що воно видає інфініті, треба придумати інший генератор
    Хоча можна і перетворити усі чила на великі значення, але виводити результат буде не естетично і буде займати немало пам'яті
    Хоча яка різниця, якщо всерівно виводить значення

--- 1/lab.js
+++ 1/lab.js
@@ -1,31 +1,50 @@
-'use strict';
+//Генератор - пошагово генерує елементи об'єкта
 
-const iterable = {
-  [Symbol.iterator]() {
-    let i = 0;
-    const iterator = {
-      next() {
-        return {
-          value: i++,
-          done: i > 3
-        };
-      }
-    };
-    return iterator;
-  }
-};
+function* fibonacciNumber(maxRange){
+    let counter = 0;
+    let firstTerm = 0n;
+    let secondTerm = 1n;
 
-// Usage
+    while(maxRange == undefined || counter < maxRange){
+        yield firstTerm;
+        let result = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = result;  
+        counter += 1;
+    }
 
-const iterator = iterable[Symbol.iterator]();
-const step1 = iterator.next();
-const step2 = iterator.next();
-const step3 = iterator.next();
-const step4 = iterator.next();
-console.log({ step1, step2, step3, step4 });
+}
+// Для тесту Перевірка
+// console.log([
+//   [...fibonacciNumber(10)],
+// ]);
 
-for (const step of iterable) {
-  console.log({ step });
+// for (const num of fibonacciNumber(1000)) { //Бесконечность не предел!!
+//     console.log(num); 
+// } 
+
+function iteratorWithTime(iterable, time){
+    
+    let list = [];
+    let sum = 0n;
+    const finishTime = Date.now() + (time*1000);
+
+    while(Date.now() < finishTime){
+        curent = iterable.next();
+        console.log(curent.value);
+        sum += curent.value;
+        list.push(curent.value)
+    }
+    return{
+        sum: sum
+    }
 }
 
-console.log({ steps: [...iterable] });
+iterable = fibonacciNumber();
+
+iterator = iteratorWithTime(iterable,10);
+
+console.log(iterator)
+
+//Мені не подобається що воно видає просто інфініті. 
+//Можна перетворити числа в BigInt, але прошавай пам'ять

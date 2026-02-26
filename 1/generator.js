8af8348dc140787b21b86d0da488e2f64c72ad38
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-02-26 16:41:23 +0200

    Число фібоначі
    Жаль немає sleep як в c
    Воно не правильно рахує числа, треба виправити

--- 1/generator.js
+++ 1/generator.js
@@ -1,12 +1,25 @@
 //Генератор - пошагово генерує елементи об'єкта
 
-function fibonacciNumber(){
-    let counter = 0;
-    next(){
-        if(counter < 3){
+function* fibonacciNumber(maxRange){
+    let counter = 2;
+    let firstTerm = 0;
+    let secondTerm = 1
 
-        }
-        return 1;
-    };
+    while(maxRange == undefined || counter < maxRange-1){ // -1 бо починається лічильник з 0 а не з 1, без нього виводилоб на 1 число більше 
+        let result = firstTerm + secondTerm;
+        yield result;
+        firstTerm = secondTerm;
+        secondTerm = result;  
+        counter += 1;
+    }
 
+}
+
+console.log([
+  [...fibonacciNumber(10)],
+]);
+
+for (const num of fibonacciNumber(1)) {
+    console.log(num); 
+    
 }

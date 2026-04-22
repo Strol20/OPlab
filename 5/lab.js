9fe73224ce0a1c3d3c208ef18473ce5a9cdf2677
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-04-23 00:47:55 +0300

    Final?

--- 5/lab.js
+++ 5/lab.js
@@ -1,20 +1,33 @@
 
 
-function asyncCallback(array, iterator, finalCallback){
+function asyncCallback(array, iterator, resultCallback){
   const results = [];
   let completed = 0;
   let hasError = false;
-  array.forEach((item, index){
-    iterator(item, (err,transformedItem)){
+  if (array.length == 0) return resultCallback(err);
+  array.forEach((item, index) =>{
+    iterator(item, (err,transformedItem) =>{
       if(hasError) return;
         if(err){
           hasError = true;
-          return finalCallback(err);
+          return resultCallback(err);
         }
-        
-    }
+        results[index] = transformedItem;
+        completed++;
+        if(completed === aray.length){
+          resultCallback(null, results)
+        }
+    });
   });
 }
 
-const aray = [1,2,3];
 
+
+const aray = [2,3,4];
+
+asyncCallback(aray, (n, next) => {
+  setTimeout(() => next(null, n * 2), 100);
+}, (err, res) => {
+  if (err) console.error(err);
+  else console.log("Callback Result:", res); // [2, 4, 6]
+});

f33cd19bd8d7257df6bfd355eafd92550a3cd816
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-04-23 00:36:37 +0300

    Праця над калбеком

--- 5/lab.js
+++ 5/lab.js
@@ -1,8 +1,19 @@
 
 
-function asyncCallback(){
-
-
+function asyncCallback(array, iterator, finalCallback){
+  const results = [];
+  let completed = 0;
+  let hasError = false;
+  array.forEach((item, index){
+    iterator(item, (err,transformedItem)){
+      if(hasError) return;
+        if(err){
+          hasError = true;
+          return finalCallback(err);
+        }
+        
+    }
+  });
 }
 
 const aray = [1,2,3];

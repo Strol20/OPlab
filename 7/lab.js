34f11e6c4b4aff485becb05983fbb41c97fd2c36
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-15 21:13:23 +0300

    lab 7 - event subscribe and describe

--- 7/lab.js
+++ 7/lab.js
@@ -1,4 +1,25 @@
 function EventEmitter() {
     const events = {}; // Тут будуть лежати наші масивчіки з функціями
 
+    return {
+        // Підписуємось на подію
+        subscribe(eventName, callback) {
+            if (!events[eventName]) {
+                events[eventName] = [];
+            }
+            
+            events[eventName].push(callback);
+            console.log(`[System] Хтось підписався на "${eventName}". Слухачів: ${events[eventName].length}`);
+
+            // Повертаємо функцію для відписки, щоб не шукати потім, що видаляти
+            return {
+                unsubscribe() {
+                    events[eventName] = events[eventName].filter(cb => cb !== callback);
+                    console.log(`[System] Відписка від "${eventName}". Залишилось: ${events[eventName].length}`);
+                }
+            };
+        },
+
+
+    };
 }

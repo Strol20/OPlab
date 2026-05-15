4d50a4af91d9d01279df221266415ce9bc5c2eb7
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-15 21:09:49 +0300

    lab 6 - generator of "hard" data

--- 6/lab.js
+++ 6/lab.js
@@ -3,5 +3,19 @@ let totalProcessedElements = 0;
 
 function DataStreamSystem() {
     
+    // 1. Генератор "важких" даних
+    async function* sourceGenerator(limit = 100000) {
+        let i = 0;
+        while (i < limit) {
+            // Типу дані з інтернету
+            yield {
+                id: i,
+                value: Math.random() * 1000,
+                timestamp: Date.now()
+            };
+            i++;
+        }
+    }
+
 
 }

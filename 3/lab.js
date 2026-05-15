0a8d4f4aa7682ad4013bd8e95249ecb4a609a8ae
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-15 21:06:57 +0300

    lab 3 - check time of life and check the need cache

--- 3/lab.js
+++ 3/lab.js
@@ -1,4 +1,31 @@
-function memoizeFunction(fn, maxRange = Infinity, policy = "NONE", time = 0){
+function memoizeFunction(fn, maxRange = Infinity, policy = "NONE", time = 0) {
 
-    
+    let cache = new Map();
+
+    return function (...args) {
+        let key = JSON.stringify(args);
+        let currentTime = Date.now();
+
+        if (cache.has(key)) {
+            let curent = cache.get(key);
+
+            // Перевірка часу життя
+            const finishTime = curent.timestamp + (time * 1000);
+            if (policy === "TIME" && time > 0 && currentTime >= finishTime) {
+                cache.delete(key);
+            } else {
+                curent.count += 1;
+                curent.timestamp = currentTime;
+
+                if (policy === "LRU") {
+                    cache.delete(key);
+                    cache.set(key, curent);
+                }
+                return curent.value;
+            }
+        }
+
+
+    }
+
 }

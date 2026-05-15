11fc3c2e66bbbaf8bbdf4f4dc8352bae5962ddff
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-15 21:10:34 +0300

    lab 6 - iterator and data processor

--- 6/lab.js
+++ 6/lab.js
@@ -17,5 +17,43 @@ function DataStreamSystem() {
         }
     }
 
+    // 2. Обробник, який працює з асинхронним ітератором
+    async function processStream(iterable, seconds) {
+        let sum = 0;
+        let count = 0;
+        const finishTime = Date.now() + (seconds * 1000);
 
+        console.log("Починаємо молотити дані...");
+
+        for await (const chunk of iterable) {
+            
+            // Фільтруємо тільки парні індекси
+            if (chunk.id % 2 === 0) {
+                sum += chunk.value;
+                count++;
+                totalProcessedElements++;
+            }
+
+            
+            if (Date.now() >= finishTime) {
+                console.log("Час вийшов, зупиняємо конвеєр");
+                break; 
+            }
+        }
+
+        // повернення лічильника
+        return {
+            totalSum: sum.toFixed(2),
+            average: count > 0 ? (sum / count).toFixed(2) : 0,
+            processedCount: count,
+            msg: "Пам'ять жива, масивів немає!"
+        };
+    }
+
+    return {
+        start: async (time) => {
+            const data = sourceGenerator(); // Створюємо потік
+            return await processStream(data, time);
+        }
+    };
 }
